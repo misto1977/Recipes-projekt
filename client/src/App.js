c251ac373a68510edb6b7dd3b3a0dcf257405b66
Milan Stoiljkovic <misto1977@yahoo.com>
2018-09-30 11:00:49 +0200

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  state = { users: [] }
+
+  componentDidMount() {
+    fetch('/users')
+      .then( res => res.json() )
+      .then( users => this.setState({ users }) );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        <ul>
+          {this.state.users.map(user =>
+            <li key={user.id}>{user.username}</li>
+          )}
+        </ul>
+        <img src="recipes/beautiful-omelet.jpg" alt="" />
+      </div>
+    );
+  }
+}
+
+export default App;
